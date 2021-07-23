8cbb6087ed5c35f9d130eaff07d762b41b78d657
zaki <zakicobrabr@gmail.com>
2021-07-23 16:34:04 +0200

    Restructuring 2

--- Cobra/src/FrontEnd/Controller/FXMLDocumentController.java
+++ Cobra/src/FrontEnd/Controller/FXMLDocumentController.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package FrontEnd.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,7 +14,7 @@ import javafx.scene.control.Label;
 
 /**
  *
- * @author basbas
+ * @author pc
  */
 public class FXMLDocumentController implements Initializable {
     
