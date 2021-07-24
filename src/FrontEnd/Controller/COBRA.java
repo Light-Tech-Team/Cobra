be1bd9276c1d589dbd93d830998155da59e28508
zaki <zakicobrabr@gmail.com>
2021-07-24 22:17:49 +0200

    Restructuring 4

--- src/FrontEnd/Controller/COBRA.java
+++ src/FrontEnd/Controller/COBRA.java
@@ -15,7 +15,7 @@ public class COBRA extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
         
         Scene scene = new Scene(root);
         
--- src/FrontEnd/Controller/HomeController.java
+++ src/FrontEnd/Controller/HomeController.java
@@ -16,7 +16,7 @@ import javafx.scene.control.Label;
  *
  * @author pc
  */
-public class FXMLDocumentController implements Initializable {
+public class HomeController implements Initializable {
     
     @FXML
     private Label label;
